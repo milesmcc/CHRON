e3a74918159b6c630e444fcce7d1fd2088a14edf
BuildTools <unconfigured@null.spigotmc.org>
2015-11-13 21:36:39 -0500

    Added /chron
    !!!

--- src/main/java/net/miblue/chron/Chron.java
+++ src/main/java/net/miblue/chron/Chron.java
@@ -1,6 +1,9 @@
 package net.miblue.chron;
 
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -43,9 +46,10 @@ public class Chron extends JavaPlugin{
 
     public class Process implements Runnable {
         public void run(){
+            reloadConfig();
             long time = System.currentTimeMillis();
             for(Job j : jobs){
-                if(!(j.getStart() > time)){
+                if(j.getStart() <= time){
                     long lastRun = getConfig().getLong("data." + j.getKey());
                     long repeat = j.getRepeat();
 
@@ -56,6 +60,91 @@ public class Chron extends JavaPlugin{
                     }
                 }
             }
+            saveConfig();
         }
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(!sender.hasPermission("chron.runjobs")){
+            sender.sendMessage(ChatColor.RED + "No permission!");
+            return true;
+        }
+        if(args.length != 1){
+            sender.sendMessage(ChatColor.DARK_PURPLE +"Available Jobs -----------------------------");
+            for(Job j : jobs){
+                sender.sendMessage(ChatColor.DARK_PURPLE + "  • " +ChatColor.LIGHT_PURPLE+ j.getKey() + " (in " + until(j) + ")");
+            }
+            sender.sendMessage(ChatColor.DARK_PURPLE + "Usage: /chron <job> // run <job>");
+            return true;
+        }
+        String job = args[0];
+        for(Job j : jobs){
+            if(j.getKey().equals(job)){
+                if(sender.hasPermission("chron.job." + j.getKey()) || sender.hasPermission("chron.runall")){
+                    j.getExecution().execute();
+                    sender.sendMessage(ChatColor.LIGHT_PURPLE + job + ChatColor.DARK_PURPLE + " successfully executed.");
+                }else{
+                    sender.sendMessage(ChatColor.RED + "You do not have access to this job. That would require: " + ChatColor.DARK_RED + "chron.runall or " + "chron.job." + j.getKey());
+                }
+                return true;
+            }
+        }
+        return true;
+    }
+
+    public String until(Job j){
+        long lastRun = getConfig().getLong("data." + j.getKey());
+        long repeat = j.getRepeat();
+
+        getLogger().info("lastrun/repeat " + lastRun + "/" + repeat);
+
+        long until = (lastRun + repeat) - System.currentTimeMillis();
+
+        int seconds = 0;
+        int minutes = 0;
+        int hours = 0;
+        int days = 0;
+        int weeks = 0;
+        if(until <= 999){
+            return "now";
+        }
+        while(until > 999){
+            seconds++;
+            if(seconds == 60){
+                minutes++;
+                seconds = 0;
+            }
+            if(minutes == 60){
+                hours++;
+                minutes = 0;
+            }
+            if(hours == 24){
+                days++;
+                hours = 0;
+            }
+            if(days == 7){
+                weeks++;
+                days = 0;
+            }
+            until -= 1000;
+        }
+        StringBuilder out = new StringBuilder();
+        if(weeks != 0){
+            out.append(weeks + " weeks, ");
+        }
+        if(days != 0){
+            out.append(days + " days, ");
+        }
+        if(hours != 0){
+            out.append(hours + " hours, ");
+        }
+        if(minutes != 0){
+            out.append(minutes + " minutes, ");
+        }
+        if(seconds != 0){
+            out.append(seconds + " seconds");
+        }
+        return out.toString().trim();
     }
 }
